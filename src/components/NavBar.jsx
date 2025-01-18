9e65d40b47dd982b27aaee21559b52cf14c13ad0
Hindarsyah <37261592+hindarsyah@users.noreply.github.com>
2025-01-18 19:38:02 +0700

    Update NavBar.jsx

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,13 +4,13 @@ import DropDown from "./Dropdown";
 export default function NavBar(){
     return(
         <>
-            <div className="navbar bg-transparent">
+            <div className="navbar bg-black">
                 <div className="navbar-start">
                     <DropDown />
                 </div>
                 <div className="navbar-end">
                     <Link className="cursor-pointer" to={'/'}>
-                        <h1 className="font-bold text-lg uppercase text-cyan-500 dark:text-cyan-50 hover:scale-105 drop-shadow-sm transition-all pr-5"><span className="text-cyan-700 dark:text-cyan-500">My</span> Portfolio</h1>
+                        <h1 className="font-bold text-lg uppercase text-black-500 dark:text-black-50 hover:scale-105 drop-shadow-sm transition-all pr-5"><span className="text-black-700 dark:text-black-500">My</span> Portfolio</h1>
                     </Link>
                 </div>
             </div>
