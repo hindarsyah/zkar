4048a88affe2405b4d78210ed04daa70df34dbcf
Hindarsyah <37261592+hindarsyah@users.noreply.github.com>
2025-01-18 19:36:05 +0700

    Update Title.jsx

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,3 +1,3 @@
 export default function Title({ title }){
-    return <h1 className="text-cyan-500 dark:text-cyan-50 text-3xl font-bold uppercase text-center">{ title }<span className="text-cyan-800 dark:text-cyan-500 border-b-4 border-cyan-950 dark:border-cyan-800"> Me</span></h1>
+    return <h1 className="text-black-500 dark:text-cyan-50 text-3xl font-bold uppercase text-center">{ title }<span className="text-black-800 dark:text-black-500 border-b-4 border-black-950 dark:border-black-800"> Me</span></h1>
 }
